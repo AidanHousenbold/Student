b9c20e59c801dd05564d1b95ce765e872f6a7748
Aidan Housenbold <HousenboldA@gmail.com>
2021-10-28 14:51:55 -0700

    Class 10/28

---
+++ Lab-Calculator/Lab-Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab-Calculator
+//
+//  Created by Aidan Housenbold on 10/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
